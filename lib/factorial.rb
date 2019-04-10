e00b484909529b980f9de08dc50be26eac041633
Jansen <v.jansen.martin@gmail.com>
2019-04-10 06:16:48 -0700

    Solve problem and explain space/time complexities.  All tests passing

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,6 +1,17 @@
 # Computes factorial of the input number and returns it
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: Linear or O(n), where n stands for input.  However many times the loop runs depends on input.
+# Space complexity: Constant 1(n), where n stands for input.  Additional space doesn't rely on input.
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError if number == nil
+
+  i = 2
+  factorial = 1
+
+  until i > number
+    factorial *= i
+
+    i += 1
+  end
+
+  return factorial
 end
